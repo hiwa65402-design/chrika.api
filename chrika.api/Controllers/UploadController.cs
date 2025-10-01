f3f10c111503f537d747dd7745463417607e0ab3
kurd12 <hiwa65402@gmail.com>
2025-10-01 16:49:17 +0300

    [Consumes("multipart/form-data")] // <-- ئەم هێڵە

--- chrika.api/Controllers/UploadController.cs
+++ chrika.api/Controllers/UploadController.cs
@@ -23,6 +23,7 @@ namespace Chrika.Api.Controllers
         // === تەنها یەک Endpointـی گشتگیر بۆ هەموو جۆرە فایلێک ===
         // POST: /api/upload
         [HttpPost]
+        [Consumes("multipart/form-data")] 
         public async Task<IActionResult> UploadFile([FromForm] IFormFile file, [FromForm] FileType fileType)
         {
             if (file == null || file.Length == 0)
